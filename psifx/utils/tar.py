3be3938e2daa39fd0452144061d5c60b335a0be0
Guillaume Rochette <rochette.guillaume@gmail.com>
2023-03-07 14:21:35 +0000

    Added verbosity argument to load() and dump() signatures. Updated the way the dir_name, internal to the archive, is added and removed.

--- psifx/utils/tar.py
+++ psifx/utils/tar.py
@@ -6,26 +6,33 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def load(path: Union[str, Path]) -> Dict[str, Any]:
+def load(
+    path: Union[str, Path],
+    verbose: Union[bool, int] = True,
+) -> Dict[str, Any]:
     dictionary = {}
     with tarfile.open(path) as tar:
-        for tarinfo in tar.getmembers():
+        for tarinfo in tqdm(tar.getmembers(), disable=not verbose):
             if tarinfo.isfile():
-                key = tarinfo.name
+                key = tarinfo.name.split("/")[-1]
                 value = tar.extractfile(tarinfo).read()
                 dictionary[key] = value
     return dictionary
 
 
-def dump(dictionary: Dict[str, Any], path: Union[str, Path]):
+def dump(
+    dictionary: Dict[str, Any],
+    path: Union[str, Path],
+    verbose: Union[bool, int] = True,
+):
     if not isinstance(path, Path):
         path = Path(path)
 
-    suffix = path.suffix.split(".")[-1]
-    dir_name = path.stem.split(".")[0]
+    suffix = path.suffix.replace(".", "")
+    dir_name = path.stem.replace(".tar", "")
 
     with tarfile.open(path, f"w:{suffix}") as tar:
-        for key, value in tqdm(dictionary.items()):
+        for key, value in tqdm(dictionary.items(), disable=not verbose):
             tarinfo = tarfile.TarInfo(f"{dir_name}/{key}")
             tarinfo.size = len(value)
             tar.addfile(tarinfo, io.BytesIO(value.encode()))

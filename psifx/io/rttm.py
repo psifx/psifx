e809a167ce24fbcdee89701c3712804febbf047c
Guillaume Rochette <rochette.guillaume@gmail.com>
2023-06-12 09:02:51 +0200

    Changed the RTTMReader/RTTMWriter to be more pythonic.

--- psifx/io/rttm.py
+++ psifx/io/rttm.py
@@ -1,16 +1,31 @@
-from typing import Sequence, Union
+from typing import Dict, List, Union
 
 from pathlib import Path
+from tqdm import tqdm
 
 import pandas as pd
-from pandas import DataFrame
+
+
+COLUMN_NAMES = [
+    "type",
+    "file_stem",
+    "channel",
+    "start",
+    "duration",
+    "orthography",
+    "speaker_type",
+    "speaker_name",
+    "confidence_score",
+    "signal_lookahead_time",
+]
 
 
 class RTTMReader(object):
     @staticmethod
     def read(
         path: Union[str, Path],
-    ) -> DataFrame:
+        verbose: bool = True,
+    ) -> List[Dict[str, Union[str, float]]]:
         path = Path(path)
 
         assert path.suffix == ".rttm"
@@ -19,70 +34,42 @@ class RTTMReader(object):
             path,
             sep=" ",
             header=None,
-            names=[
-                "type",
-                "file_stem",
-                "channel",
-                "start",
-                "duration",
-                "orthography",
-                "speaker_type",
-                "speaker_name",
-                "confidence_score",
-                "signal_lookahead_time",
-            ],
+            names=COLUMN_NAMES,
         )
-        dataframe["end"] = dataframe["start"] + dataframe["duration"]
-
-        return dataframe
+        segments = [
+            {
+                "type": dataframe.iloc[index]["type"],
+                "file_stem": dataframe.iloc[index]["file_stem"],
+                "channel": dataframe.iloc[index]["channel"],
+                "start": dataframe.iloc[index]["start"],
+                "duration": dataframe.iloc[index]["duration"],
+                "orthography": dataframe.iloc[index]["orthography"],
+                "speaker_type": dataframe.iloc[index]["speaker_type"],
+                "speaker_name": dataframe.iloc[index]["speaker_name"],
+                "confidence_score": dataframe.iloc[index]["confidence_score"],
+                "signal_lookahead_time": dataframe.iloc[index]["signal_lookahead_time"],
+            }
+            for index in tqdm(
+                range(dataframe.shape[0]),
+                desc="Decoding",
+                disable=not verbose,
+            )
+        ]
+        return segments
 
 
 class RTTMWriter(object):
     @staticmethod
     def write(
         path: Union[str, Path],
-        type: Sequence[str],
-        file_stem: Sequence[str],
-        channel: Sequence[int],
-        start: Sequence[float],
-        duration: Sequence[float],
-        orthography: Sequence[str],
-        speaker_type: Sequence[str],
-        speaker_name: Sequence[str],
-        confidence_score: Sequence[Union[float, str]],
-        signal_lookahead_time: Sequence[Union[float, str]],
+        segments: List[Dict[str, Union[str, float]]],
         overwrite: bool = False,
     ):
         path = Path(path)
 
         assert path.suffix == ".rttm"
-        assert (
-            len(type)
-            == len(file_stem)
-            == len(channel)
-            == len(start)
-            == len(duration)
-            == len(orthography)
-            == len(speaker_type)
-            == len(speaker_name)
-            == len(confidence_score)
-            == len(signal_lookahead_time)
-        )
 
-        dataframe = pd.DataFrame.from_dict(
-            {
-                "type": type,
-                "file_stem": file_stem,
-                "channel": channel,
-                "start": start,
-                "duration": duration,
-                "orthography": orthography,
-                "speaker_type": speaker_type,
-                "speaker_name": speaker_name,
-                "confidence_score": confidence_score,
-                "signal_lookahead_time": signal_lookahead_time,
-            }
-        )
+        dataframe = pd.DataFrame.from_records(segments)
 
         if path.exists():
             if overwrite:
@@ -90,4 +77,12 @@ class RTTMWriter(object):
             else:
                 raise FileExistsError(path)
         path.parent.mkdir(parents=True, exist_ok=True)
-        dataframe.to_csv(path, sep=" ", header=False, float_format="%.3f")
+        dataframe.to_csv(
+            path,
+            sep=" ",
+            na_rep="<NA>",
+            columns=COLUMN_NAMES,
+            header=False,
+            index=False,
+            float_format="%.3f",
+        )

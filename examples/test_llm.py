958f8390f2b6bf4582ba9c894517c14f6e191cf7
Mathieu Rochat <mathieu.louis.rochat@gmail.com>
2024-06-11 10:07:17 +0200

    Edit examples

--- examples/test_llm.py
+++ examples/test_llm.py
@@ -25,16 +25,28 @@ args = ["main", "text", "chat", "--model", "llama3", "--prompt", "chat_history.t
 # Chose any specification for the llm with a .json config file
 args = ["main", "text", "chat", "--model", "ollama_llama3_cpu.json", "--prompt", "template.txt"]
 
+
 """
 TASC SEGMENTING
 """
 
 # Segmenting for .vtt files
-args = ["main", "text", "tasc", "--model", "llama3", "--speaker", "student_normalized.wav", "--transcription", "transcription.vtt",
+args = ["main", "text", "tasc", "segment", "--model", "llama3", "--speaker", "student_normalized.wav",
+        "--transcription", "transcription.vtt",
         "--segmentation", "segmentation.vtt", "--overwrite"]
+
 # Segmenting for .csv files
-args = ["main", "text", "tasc", "--model", "llama3", "--speaker", "Reply", "--transcription", "transcription.csv",
+args = ["main", "text", "tasc", "segment", "--model", "llama3", "--transcription", "transcription.csv",
         "--segmentation", "segmentation.csv", "--overwrite"]
+
+
+"""
+INSTRUCTION
+"""
+
+args = ["main", "text", "instruction", "--model", "llama3", "--input", "input.csv",
+        "--output", "output.csv", "--overwrite"]
+
 
 with patch("sys.argv", args):
     command.main()

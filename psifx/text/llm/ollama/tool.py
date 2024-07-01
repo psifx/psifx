bb878198e1c514bb8553beb89d44ec459e2f069e
mathieu rochat <mathieu.louis.rochat@gmail.com>
2024-07-01 15:59:58 +0200

    Automatic model download for ollama

--- psifx/text/llm/ollama/tool.py
+++ psifx/text/llm/ollama/tool.py
@@ -12,4 +12,30 @@ class WrapperChatOllama(ChatOllama):
 
 
 def get_ollama(model='llama3', **kwargs):
-    return WrapperChatOllama(model=model, **kwargs)
+    if model not in ([available_model['name'] for available_model in ollama.list()['models']]):
+        pull_model(model)
+
+    return ChatOllama(model=model, **kwargs)
+
+
+def pull_model(model):
+    print(f"pulling {model}")
+    pbar = None
+    current_digest = None
+    for data in ollama.pull(model, stream=True):
+        if data.get('digest') is None:
+            continue
+        if data['digest'] != current_digest:
+            if pbar:
+                pbar.close()
+            current_digest = data['digest']
+            total_size = data['total']
+            pbar = tqdm(total=total_size, desc=f"{data['status']}", unit="B", unit_scale=True)
+
+        completed_size = data.get('completed', 0)
+        pbar.n = completed_size
+        pbar.refresh()
+
+    if pbar:
+        pbar.close()
+    print(f"successfully pulled {model}")

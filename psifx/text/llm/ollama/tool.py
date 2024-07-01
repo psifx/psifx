f1e79bc54c9487c6efa3395e81a366f27f2cd23a
M-Rochat <96523860+M-Rochat@users.noreply.github.com>
2024-07-01 17:41:38 +0200

    Fix import error

--- psifx/text/llm/ollama/tool.py
+++ psifx/text/llm/ollama/tool.py
@@ -1,14 +1,5 @@
 from langchain_community.chat_models import ChatOllama
-
-
-class WrapperChatOllama(ChatOllama):
-    def generate(self, *args, **kwargs):
-        try:
-            return super().generate(*args, **kwargs)
-        except Exception as e:
-            print("Make sure you downloaded the model from Ollama")
-            print(e)
-            raise e
+import ollama
 
 
 def get_ollama(model='llama3', **kwargs):

e27b69cce878ca68193632206ec97d94c4b4e1a1
mathieu rochat <mathieu.louis.rochat@gmail.com>
2024-07-26 14:33:45 +0200

    Added .txt support for instruction

--- psifx/text/instruction/tool.py
+++ psifx/text/instruction/tool.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from langchain_core.runnables import RunnableLambda
 
+from psifx.io.txt import TxtWriter, TxtReader
 from psifx.io.csv import CsvWriter, CsvReader
 from psifx.tool import Tool
 from tqdm.auto import tqdm
@@ -15,13 +16,25 @@ class InstructionTool(Tool):
     def __init__(self, chain, overwrite: bool = False,
                  verbose: Union[bool, int] = True):
         super().__init__(device="?", overwrite=overwrite, verbose=verbose)
-        self.chain = RunnableLambda(lambda x: x.to_dict()) | chain
+        self.chain = chain
+
+    def apply_to_txt(self, input_path, output_path, output_column='result'):
+        TxtWriter.check(output_path, overwrite=self.overwrite)
+        text = TxtReader.read(input_path)
+        result = self.chain.invoke({'text': text})
+
+        TxtWriter.write(
+            content=result,
+            path=output_path,
+            overwrite=self.overwrite
+        )
 
     def apply_to_csv(self, input_path, output_path, output_column='result'):
-        tqdm.pandas(desc="Processing")
         CsvWriter.check(output_path, overwrite=self.overwrite)
         df = CsvReader.read(path=input_path)
-        df[output_column] = df.progress_apply(self.chain.invoke, axis=1)
+        df_chain = RunnableLambda(lambda x: x.to_dict()) | self.chain
+        tqdm.pandas(desc="Processing")
+        df[output_column] = df.progress_apply(df_chain.invoke, axis=1)
         CsvWriter.write(
             df=df,
             path=output_path,

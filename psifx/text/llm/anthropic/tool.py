608e09b7a35a2c92352f4d55a86537d737b4955a
mathieu rochat <mathieu.louis.rochat@gmail.com>
2024-07-31 14:17:01 +0200

    Moved imports

--- psifx/text/llm/anthropic/tool.py
+++ psifx/text/llm/anthropic/tool.py
@@ -1,10 +1,10 @@
 import os
-from langchain_anthropic import ChatAnthropic
 import getpass
 
 
 def get_anthropic(model='claude-3-5-sonnet-20240620', api_key=None, temperature=0, max_tokens=None, timeout=None,
                   max_retries=2, **kwargs):
+    from langchain_anthropic import ChatAnthropic
     api_key = api_key or getpass.getpass("Enter your Anthropic API key: ")
     os.environ["ANTHROPIC_API_KEY"] = api_key
     return ChatOpenAI(
--- psifx/text/llm/openai/tool.py
+++ psifx/text/llm/openai/tool.py
@@ -1,8 +1,8 @@
-from langchain_openai import ChatOpenAI
 import getpass
 
 
 def get_openai(model='gpt-4o', api_key=None, temperature=0, max_tokens=None, timeout=None, max_retries=2, **kwargs):
+    from langchain_openai import ChatOpenAI
     api_key = api_key or getpass.getpass("Enter your OpenAI API key: ")
     return ChatOpenAI(
         model=model,
